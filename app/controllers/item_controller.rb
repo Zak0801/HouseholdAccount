2f9a9a81078285e7b720296e9dfb2300a7e2f575
Kazuki Shimizu <s.k24310723@gmail.com>
2019-06-17 23:47:33 +0900

    Add a Submit function

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -4,5 +4,16 @@ class ItemController < ApplicationController
   end
 
   def index
+    @items = Item.all
   end
 
+  def create
+    @item = Item.new(price: params[:price], category: params[:category], using_month: params[:month], using_day: params[:day])
+    if @item.save
+      redirect_to("/item/index")
+    else
+      render("item/new")
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'item#new'
   get 'item/index' => "item#index"
+  post 'item/create' => "item#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
