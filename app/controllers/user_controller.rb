add3bf899a6b384d4874847d10a0f658ac884e5b
Kazuki Shimizu <s.k24310723@gmail.com>
2019-06-18 02:48:55 +0900

    Add to a login  page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,28 @@
 class UserController < ApplicationController
   def new
-    @user = User.new(name: params[:myname], password: params[:password])
-    @user.save
   end
+
+  def create
+    @user = User.new(name: params[:myname], password: params[:password])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to("/item/index")
+    else
+      render("user/new")
+    end
+  end
+
+  def login_form
+  end
+
+  def login
+    @user = User.find_by(password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to("/item/index")
+    else
+      render("user/login_form")
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
-  get 'user/new' => "user#new"
-
   root 'item#new'
-  get 'item/index' => "item#index"
-  post 'item/create' => "item#create"
-  get 'item/:id/edit' => "item#edit_form"
-  post 'item/:id/edit' => "item#edit"
-  post 'item/:id/destroy' => "item#destroy"
+  get '/item/index' => "item#index"
+  post '/item/create' => "item#create"
+  get '/item/:id/edit' => "item#edit_form"
+  post '/item/:id/edit' => "item#edit"
+  post '/item/:id/destroy' => "item#destroy"
   get '/item/month/:using_month' => "item#month"
+
+  get '/user/new' => "user#new"
+  post '/user/create' => "user#create"
+  get '/login' => "user#login_form"
+  post '/login' => "user#login"
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
