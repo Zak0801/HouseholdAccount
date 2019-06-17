4072579ce34aa07352eb9b7a345d45620e2cc83a
Kazuki Shimizu <s.k24310723@gmail.com>
2019-06-18 00:23:32 +0900

    Add a edit item function

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -16,6 +16,23 @@ class ItemController < ApplicationController
     end
   end
 
+  def edit_form
+    @item = Item.find_by(id: params[:id])
+  end
+
+  def edit
+    @item = Item.find_by(id: params[:id])
+    @item.price = params[:price]
+    @item.category = params[:category]
+    @item.using_month = params[:month]
+    @item.using_day = params[:day]
+    if @item.save
+      redirect_to("/item/index")
+    else
+      render("item/edit_form")
+    end 
+  end
+
   def destroy
     @item = Item.find_by(id: params[:id])
     @item.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'item#new'
   get 'item/index' => "item#index"
   post 'item/create' => "item#create"
+  get 'item/:id/edit' => "item#edit_form"
+  post 'item/:id/edit' => "item#edit"
   post 'item/:id/destroy' => "item#destroy"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
