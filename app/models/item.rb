963e99591f035a6b9b0a4c972aed90a2ce7e4875
Kazuki Shimizu <s.k24310723@gmail.com>
2019-06-18 00:27:55 +0900

    Write validations of item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+    validates :price, {presence: true}
+    validates :category, {presence: true}
+    validates :using_month, {presence: true}
+    validates :using_day, {presence: true}
+
 end
