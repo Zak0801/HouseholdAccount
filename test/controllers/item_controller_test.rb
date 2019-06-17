10e336bb1c240b1630ea61383d01cc43be8e971e
Kazuki Shimizu <s.k24310723@gmail.com>
2019-06-18 02:06:55 +0900

    Add to a controller test

--- test/controllers/item_controller_test.rb
+++ test/controllers/item_controller_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class ItemControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get item_new_url
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get item_index_url
     assert_response :success
   end
 
